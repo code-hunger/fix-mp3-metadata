4e21ee344b725dc2d0e971ee1141b4988a0ee23a
code-hunger <code-hunger@users.noreply.github.com>
2015-06-30 00:12:15 +0300

    Create extractMP3Data.pl

---
+++ extractMP3Data.pl
@@ -0,0 +1,22 @@
+#!perl
+use 5.010;
+use strict;
+use warnings;
+use MP3::Tag;
+opendir DIR, ".";
+$ENV{'MP3TAG_ENCODE_V1_DEFAULT'} = 'UTF-8';
+$ENV{'MP3TAG_ENCODE_FILES_DEFAULT'} = 'UTF-8';
+MP3::Tag->config(write_v24 => 1);
+say "Enter album name:";
+chomp (my $album = <>);
+my $i;
+while (my $file = readdir(DIR)) {
+	next unless $file =~ /^(?:\d+\.\s*)?(?<artist>.+?)\-(?<title>.+)\.mp3$/;
+	my ($artist, $title) = ($+{artist}, $+{title});
+	my $mp3 = MP3::Tag->new($file);
+	$mp3->title_set($title);
+	$mp3->artist_set($artist);
+	$mp3->album_set($album);
+	$mp3->update_tags();
+	say "Working on file $file. Data: title = ".$mp3->title().", artist = ".$mp3->artist();
+}
