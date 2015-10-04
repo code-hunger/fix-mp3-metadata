ccf3362af3ae9d5888d5c4eac2e258d3dd5f6d38
code-hunger <code-hunger@users.noreply.github.com>
2015-10-04 20:17:30 +0300

    Update extractMP3Data.pl
    Shebang edited, need of perl 5.010 removed, unused variable $i removed.

--- extractMP3Data.pl
+++ extractMP3Data.pl
@@ -1,15 +1,14 @@
-#!perl
-use 5.010;
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use MP3::Tag;
+
 opendir DIR, ".";
 $ENV{'MP3TAG_ENCODE_V1_DEFAULT'} = 'UTF-8';
 $ENV{'MP3TAG_ENCODE_FILES_DEFAULT'} = 'UTF-8';
 MP3::Tag->config(write_v24 => 1);
-say "Enter album name:";
+print "Enter album name:\n";
 chomp (my $album = <>);
-my $i;
 while (my $file = readdir(DIR)) {
 	next unless $file =~ /^(?:\d+\.\s*)?(?<artist>.+?)\-(?<title>.+)\.mp3$/;
 	my ($artist, $title) = ($+{artist}, $+{title});
@@ -18,5 +17,5 @@ while (my $file = readdir(DIR)) {
 	$mp3->artist_set($artist);
 	$mp3->album_set($album);
 	$mp3->update_tags();
-	say "Working on file $file. Data: title = ".$mp3->title().", artist = ".$mp3->artist();
+	print "Working on file $file. Data: title = ".$mp3->title().", artist = ".$mp3->artist(), "\n";
 }
